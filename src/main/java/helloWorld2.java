9f2d14458cf0a5b05d6fc4fb8c34d8fc9e0e2f3f
JackWalton1 <jwalto02@calpoly.edu>
2022-10-14 11:18:42 +0200

    first commit

---
+++ src/main/java/helloWorld2.java
@@ -0,0 +1,6 @@
+public class helloWorld2 {
+        public static void main(String[] args){
+            String str = "Hello World!! Pls push to git";
+            System.out.println(str);
+        }
+}
